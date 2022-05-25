7e19b77a54d0292470208259f61a5a610c6e373a
Krystof Mitka <krystof.mitka@seznam.cz>
2022-05-25 10:43:44 +0200

    :art: Add Sass

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+const pathToApp = './src/main/webapp'
+
+gulp.task('styles', () => {
+    return gulp.src(pathToApp + '/styles/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(pathToApp + '/'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch(pathToApp + '/styles/**/*.sass', (done) => {
+        gulp.series(['clean', 'styles'])(done);
+    });
+});
+
+gulp.task('clean', () => {
+    return del([
+        pathToApp + '/index.css',
+    ]);
+});
+
+gulp.task('default', gulp.series(['styles', 'watch']));
