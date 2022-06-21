f17fecf552be9655ee3e4efad4621c5eac7b15c4
Miki Vanoušek <vanousekmikulas@gmail.com>
2022-06-21 11:28:55 +0200

    :art: create the schedule after vessel

--- src/main/webapp/script/api.js
+++ src/main/webapp/script/api.js
@@ -3,13 +3,13 @@ import Requests from "./requests.js";
 class VisolApi {
   static portId = 1
 
-  static getTerminals() {
-    return Requests.getData(`/ports/${VisolApi.portId}/terminals`);
-  }
+  static getTerminals = () => Requests.getData(`/ports/${VisolApi.portId}/terminals`);
 
-  static postVessel(vessel) {
-    return Requests.postData('/vessels', vessel)
-  }
+  static postVessel = (vessel) => Requests.postData('/vessels', vessel)
+
+  static putSchedule = (vessel_id, schedule) => Requests.putData(`/vessels/${vessel_id}/schedule`, schedule)
+
+  static formatLocalDateTime = (dt) => new Date(Date.parse(dt)).toISOString().substring(0,19) + 'Z'
 }
 
 export default VisolApi
--- src/main/webapp/script/components/error-toast.js
+++ src/main/webapp/script/components/error-toast.js
@@ -1,3 +1,4 @@
+// TODO Miki customize text
 class ErrorToast extends HTMLElement {
   static id = "error-toast"
   connectedCallback() {
@@ -6,7 +7,7 @@ class ErrorToast extends HTMLElement {
   <div class="toast align-items-center text-white bg-danger border-0" role="alert" 
     aria-live="assertive" aria-atomic="true" id="${ErrorToast.id}">
     <div class="d-flex">
-      <div class="toast-body">
+      <div class="toast-body" id="toast-body">
         <b> A request failed! </b> Try reloading the page!
       </div>
       <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
@@ -15,7 +16,10 @@ class ErrorToast extends HTMLElement {
 </div>`
   }
 
-  static get() { return bootstrap.Toast.getOrCreateInstance(document.getElementById(ErrorToast.id))}
+  static show(content = "<b> A request failed! </b> Try reloading the page!") {
+    document.getElementById("toast-body").innerHTML = content
+    bootstrap.Toast.getOrCreateInstance(document.getElementById(ErrorToast.id)).show()
+  }
 }
 
 customElements.define('error-toast', ErrorToast);
