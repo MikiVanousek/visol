c71aafb17ebae7f0acf090ef6b7828658b1b813c
Krystof Mitka <krystof.mitka@seznam.cz>
2022-06-21 12:38:21 +0200

    :lipstick: planner schedule

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const del = require('del');
-const pathToApp = './src/main/webapp'
+const pathToApp = './src/main/webapp';
 
 gulp.task('styles', () => {
   return gulp.src(pathToApp + '/styles/index.sass')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(pathToApp + '/'));
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest(pathToApp + '/'));
 });
 
 gulp.task('bootstrap', () => {
   return gulp.src(pathToApp + '/styles/bootstrap.sass')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(pathToApp + '/'));
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest(pathToApp + '/'));
 });
 
 gulp.task('watch', () => {
---
+++ src/main/webapp/script/components/vessel-card.js
@@ -0,0 +1,71 @@
+// eslint-disable-next-line no-unused-vars
+import IconCircle from './icon-circle.js';
+
+class VesselCard extends HTMLElement {
+  static VIEW = {
+    automatic: 'automatic',
+    manual: 'manual',
+    unscheduled: 'unscheduled',
+  };
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const view = this.getAttribute('view');
+    const name = this.getAttribute('name');
+    // const id = this.getAttribute('id');
+
+    this.innerHTML = `
+    <div class="card view-${view}"
+        data-bs-toggle="modal" 
+        data-bs-target="#update-modal"
+        >
+        <div class="card-info">
+          <icon-circle
+            name="ship"
+            view=${IconCircle.VIEW.primary}>
+          </icon-circle>
+          <div class="card-info-in">
+            <h6 class="card-info-heading">${name}</h6>
+            ${(this.hasAttribute('arrival') ||
+              this.hasAttribute('departure')) ? this.getDescription() : ''}
+          </div>
+        </div>
+        ${view === VesselCard.VIEW.manual ? this.getFooter() : ''}
+    </div>
+    
+    
+    `;
+    // this.addEventListener('click', this.cardPopup);
+  }
+
+  cardPopup(e) {
+    // Miki's popup for editing vessel
+  }
+
+  getDescription() {
+    return `<div class="card-info-description">
+    ${this.getAttribute('arrival') + ' - ' + this.getAttribute('departure') +
+     ' • ' + this.capitalizeFirstLetter(this.getAttribute('view'))}
+    </div>`;
+  }
+
+  getFooter() {
+    return `<div class="card-footer bg-transparent border-0">
+              <icon-plain
+                  name="hand-paper"
+                  view=${IconCircle.VIEW.primary}
+              ></icon-plain>
+           </div>`;
+  }
+
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+}
+
+customElements.define('vessel-card', VesselCard);
+
+export default VesselCard;
