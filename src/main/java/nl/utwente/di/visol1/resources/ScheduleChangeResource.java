a75cf9bd1ea11ba4579a87f88aaa0d6999608918
KleineKabouter <n.vanduijl@student.utwente.nl>
2022-06-23 10:21:39 +0200

    :fire: Removed methods from old API

--- src/main/java/nl/utwente/di/visol1/resources/ScheduleChangeResource.java
+++ src/main/java/nl/utwente/di/visol1/resources/ScheduleChangeResource.java
@@ -1,9 +1,7 @@
 package nl.utwente.di.visol1.resources;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -33,19 +31,6 @@ public class ScheduleChangeResource {
 		this.uriInfo = uriInfo;
 		this.request = request;
 		this.id = Integer.parseInt(id);
-	}
-
-	@POST
-	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response createScheduleChange(ScheduleChange schange) {
-		schange.setVessel(id);
-		ScheduleChange createdChange = ScheduleChangeDao.createScheduleChange(schange);
-		if (createdChange != null) {
-			return Response.status(Response.Status.OK).entity(createdChange).build();
-		} else {
-			return Response.status(Response.Status.NOT_ACCEPTABLE).build();
-		}
 	}
 
 	@GET
